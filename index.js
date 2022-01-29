4831e843f9c602d1ba3e15bc0b14b4ad4d290d95
davidplatino <dsplatino@gmail.com>
2022-01-29 14:08:46 -0300

    Atualização de teste

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 //testando
+
+const express = require('express'); //Controla e acessar rotas, middleware e recursos
+const app = express();
+
+app.get('/projects', (request, response) => {    
+	return response.send('Hello world!');
+});
+//método p/ configurar a porta de acesso da app, https://localhost:3333
+
+app.listen(3333);
